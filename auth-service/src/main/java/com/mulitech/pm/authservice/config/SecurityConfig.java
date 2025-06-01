3267dd1a147d7ebd2fd791c095aa93611113766b
mulitha <mulithanuwan@gmail.com>
2025-06-01 11:04:56 +0800

    Added JwtValidationGatewayFilterFactory to intercept all the api calls for non auth services and validated for valid jwt token. This validation exempt auth login api call.

--- auth-service/src/main/java/com/mulitech/pm/authservice/config/SecurityConfig.java
+++ auth-service/src/main/java/com/mulitech/pm/authservice/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.mulitech.pm.authservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
